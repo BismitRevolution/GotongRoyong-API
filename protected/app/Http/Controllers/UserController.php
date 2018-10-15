f8a50fc4177b77a750482e00d72ed1e6cbf3e59d
nugraha-aditya <adityapradana73@gmail.com>
2018-10-15 12:40:16 +0700

    update user by dana

--- protected/app/Http/Controllers/UserController.php
+++ protected/app/Http/Controllers/UserController.php
@@ -10,6 +10,8 @@ use App\Http\Controllers\User\UserList as UList;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use DateTime;
+use Carbon\Carbon;
+
 
 
 
@@ -231,6 +233,46 @@ class UserController extends Controller
         500);
 
     }
+
+  public function updateImgProfile(Request $request)
+  {
+    if ($request->has('image_profile')) {
+
+      $imgUrl = $request->get('image_profile');
+      $destinationPath = 'Uploads/user-photo-profile/'.Carbon::now()->timestamp;
+
+      $moves = $request->file('image_profile')
+          ->move($destinationPath,
+              $request->file('image_profile')
+                  ->getClientOriginalName());
+      $url_file = "Uploads/user-photo-profile/".Carbon::now()->timestamp."/{$request->file('image_profile')->getClientOriginalName()}";
+
+      $user = Auth::guard('api')->user();
+      if($user){
+        $user->image_profile = $url_file;
+        $user->save();
+
+        return response()->json([
+          'success' => true,
+          'message' => 'Update user image profile success',
+          'data' => $user->image_profile],
+          200);
+      }
+      return response()->json([
+        'success' => false,
+        'message' => 'Update user image failure',
+        'data' => 'Already logged out/Token false'],
+        500);
+    }
+
+    return response()->json([
+      'success' => false,
+      'message' => 'Update user image failure',
+      'data' => 'No file image'],
+      500);
+
+
+  }
 
     // public function updateUserPahlawan(Request $request)
     // {
